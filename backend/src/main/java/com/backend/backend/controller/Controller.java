4966d473fddffc44677a91387184f4e9fa9632a0
Céccile <simoncecilepro@outlook.fr>
2021-10-21 20:26:49 +0200

    retour

--- backend/src/main/java/com/backend/backend/controller/Controller.java
+++ backend/src/main/java/com/backend/backend/controller/Controller.java
@@ -3,81 +3,68 @@ package com.backend.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import com.backend.backend.model.Post;
+import com.backend.backend.services.PostService;
 
 // // @RestController indique à Spring que cette classe est un bean & 
-// insérer le retour de la méthode au format JSON dans le corps de la réponse HTTP
-// MainController appeler les méthodes pour communiquer avec la base de données.
+// insérer le retour de la méthode dans le corps de la réponse HTTP
 
-@RestController (path= " 'http://localhost:8080/api/posts'")
+@RestController 
+
+@RequestMapping("/api/posts")
   public class Controller {
 
     
     // injeter interface
   @Autowired
     private PostService postService;
-
 }
   
-@RequestMapping
-  public List <post> List()
-
-
-  @GetMapping (path= "/{id}")
-  public Post get (@PathVariable("id") Long id){
-    Post result = postService.get();
-    return null;
+@GetMapping ("")
+  public List <Post> list(){
+    return postService.list();
   }
 
 
-  @GetMapping (path= "/{id}, 
-    public List<ost> getAll
+  @GetMapping (path= "/{id}")
+    public Post get (@PathVariable("id") Long id){
+      Post result = postService.get(id);
+      if (result == null){
+        throw new NoSuchObjectException();
+      }
+    return result;
+  }
+
+@DeleteMapping(path= "/{id}")
+  public Post delete (@PathVariable("id") Long id){
+    Post result = postService.delete(id);
+    return result;
+
+  }
 
 
+  @GetMapping(path= "/posts?title={title}")
+    public Post get (@PathVariable("title") String title){
+      Post result = postService.findByTitle(title);
+      return result;
+    }
 
-  // value method est un alias pour la méthode path.
+    @PostMapping("")
+      public Long create (@RequestBody Post post){
+        Long result = postService.createNewPost(post);
+        return result;
+      }
 
-  // @RequestMapping(value = "/path", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT)
-  //   public void handleRequet() {
+    @PutMapping(path= "/{id}")
+    public Post put (@PathVariable("id") Long id){
+      Post result = postService.put(id);
+      return result;
+    }
     
-  //   }
 
-
-
-
-
-
-
-  
-  // @RequestMapping("/users")
-  // public class UserController {
-  
-  //     @PostMapping
-  //     public void createUser(Request request) {
-  //         // POST /users
-  //         // create a user
-  //     }
-  
-  //     @GetMapping
-  //     public Users getUsers(Request request) {
-  //         // GET /users
-  //         // get users
-  //     }
-  
-  //     @GetMapping("/{id}")
-  //     public Users getUserById(@PathVariable long id) {
-  //         // GET /users/1
-  //         // get user by id
-  //     }
-  // }
-
-
-
-
-
-  // The @GetMapping maps a / root path from a GET request to the index()method
 
